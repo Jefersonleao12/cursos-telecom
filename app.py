df3b443ce55252dfe9735b30c3f7d63a970bdc00
Jefersonleao12 <130243961+Jefersonleao12@users.noreply.github.com>
2026-08-01 08:39:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -52,9 +52,9 @@ st.markdown(
         div[data-testid="stImage"] img { overflow: visible; }
         section[data-testid="stSidebar"] .block-container { padding-top: 1.2rem; }
         section[data-testid="stSidebar"] {
-            min-width: 250px !important;
-            max-width: 250px !important;
-            width: 250px !important;
+            min-width: 230px !important;
+            max-width: 230px !important;
+            width: 230px !important;
         }
     </style>
     """,
