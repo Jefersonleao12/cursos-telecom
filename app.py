cdfec648951ae22cfdc95e5f237cd774f9fcc316
Jefersonleao12 <130243961+Jefersonleao12@users.noreply.github.com>
2026-08-05 08:22:05 -0400

    Import Streamlit and display version
    Added Streamlit import and version display.

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ Responsável por:
 
 Para rodar localmente:  streamlit run app.py
 """
+import streamlit as st
+
+st.write(st.__version__)
+
 import base64
 import streamlit as st
 from pathlib import Path
